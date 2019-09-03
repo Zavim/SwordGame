26032acb534e202f1dee02f7a1ae1541876af569
Zavim <z.mclean@vikes.csuohio.edu>
2019-09-02 21:20:37 -0400

    main menu with hovering, no clickables

--- swordgame/sketch.js
+++ swordgame/sketch.js
@@ -1,21 +1,67 @@
-var spr;
+var buttonW = 80;
+var buttonH = 40;
+var canvasW = 256;
+var canvasH = 192;
+var pad = 6;
 
 function setup() {
-    createCanvas(400, 400);
-    backgroundColor = color(100, 50, 150);
-    background(backgroundColor);
-    spr = createSprite(
-        width / 2, height / 2, 40, 40);
-    spr.shapeColor = color(255);
-    spr.velocity.y = 0.5;
+    createCanvas(canvasW, canvasH);
+    background('#222222');
+    logInB = new Clickable(canvasW / 2 - buttonW / 2, pad);
+    logInB.width = buttonW;
+    logInB.height = buttonH;
+    logInB.text = "Log In";
+
+    logInB.onHover = function () {
+        this.color = '#AAAAFF';
+    }
+
+    logInB.onOutside = function () {
+        this.color = '#FFFFFF';
+    }
+
+    myInfoB = new Clickable(canvasW / 2 - buttonW / 2, buttonH + 2 * pad);
+    myInfoB.width = buttonW;
+    myInfoB.height = buttonH;
+    myInfoB.text = "My Info";
+
+    myInfoB.onHover = function () {
+        this.color = '#AAAAFF';
+    }
+    myInfoB.onOutside = function () {
+        this.color = '#FFFFFF';
+    }
+
+    practiceB = new Clickable(canvasW / 2 - buttonW / 2, (2 * buttonH + 3 * pad));
+    practiceB.width = buttonW;
+    practiceB.height = buttonH;
+    practiceB.text = "Practice";
+
+    practiceB.onHover = function () {
+        this.color = '#AAAAFF';
+    }
+
+    practiceB.onOutside = function () {
+        this.color = '#FFFFFF';
+    }
+
+    tournamentB = new Clickable(canvasW / 2 - buttonW / 2, (3 * buttonH + 4 * pad));
+    tournamentB.width = buttonW;
+    tournamentB.height = buttonH;
+    tournamentB.text = "Tournament";
+
+    tournamentB.onHover = function () {
+        this.color = '#AAAAFF';
+    }
+
+    tournamentB.onOutside = function () {
+        this.color = '#FFFFFF';
+    }
 }
 
 function draw() {
-    background(backgroundColor);
-    drawSprites();
-}
-
-function mousePressed() {
-    spr.position.x = mouseX;
-    spr.position.y = mouseY;
+    logInB.draw();
+    myInfoB.draw();
+    practiceB.draw();
+    tournamentB.draw();
 }
