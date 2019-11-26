37e8a13cab51d2b4bb0ed567c59d95e616897c35
Zavim <z.mclean@vikes.csuohio.edu>
2019-11-26 08:48:36 -0500

    we got blue

--- swordgame/cc.js
+++ swordgame/cc.js
@@ -1,17 +1,14 @@
 var face, head;
 
-function preload() {
-    face = loadAnimation('sprites/CC_test_face.png', 'sprites/CC_test_face1.png');
-    head = loadAnimation('sprites/CC_head_outline.png');
-}
+// function preload() {
+//     face = loadAnimation('sprites/CC_test_face.png', 'sprites/CC_test_face1.png');
+//     head = loadAnimation('sprites/CC_head_outline.png');
+// }
 
 function setup() {
-    createCanvas(800, 300);
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    background(255, 255, 255);
+    background(100, 255, 255);
 }
-
-animation(face, 300, 150);
-animation(head, 300, 150);
