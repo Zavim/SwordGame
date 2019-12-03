fcf490cffe1ac4196be43a224619a0b94efc337c
Zavim <z.mclean@vikes.csuohio.edu>
2019-12-03 13:31:33 -0500

    arrows on screen

--- tst/facepicker.js
+++ tst/facepicker.js
@@ -1,20 +1,35 @@
 var face;
-let head;
+var head;
+
+
 
 function preload() {
     head = loadImage('assets/CC_test_head_outline.png');
-    // head = loadImage('https://i.imgur.com/GzSKmjW.png');
-    //   face = loadAnimation("assets/CC_test_face.png", "assets/CC_test_face1.png")
+    // face = loadAnimation("assets/CC_test_face.png", "assets/CC_test_face1.png")
+    face = loadImage("assets/CC_test_face.png");
+    next_arrow = loadImage('assets/next_arrow.png');
+    prev_arrow = loadImage('assets/prev_arrow.png');
+
+
 }
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
+    imageMode(CENTER);
     image(head, 0, 0);
+    image(face, 0, 0);
+    image(next_arrow, 0, 0);
+    image(prev_arrow, 0, 0);
+
 }
 
 function draw() {
     background(220);
-    image(head, 0, 0);
-    // animation(face, 100, 100);
+
+    image(head, windowWidth / 2, windowHeight / 2);
+    image(face, windowWidth / 2, windowHeight / 2);
+    image(next_arrow, windowWidth / 2 + 230, windowHeight / 2);
+    image(prev_arrow, windowWidth / 2 - 230, windowHeight / 2);
+    // animation(face, windowWidth / 2, windowHeight / 2);
     // animation(glitch, 200, 100);
 }
