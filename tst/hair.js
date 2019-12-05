25464c9a8fdba4ce1cb74efee7086e3a5f754db0
Zavim <z.mclean@vikes.csuohio.edu>
2019-12-05 13:34:13 -0500

    new pattern!

---
+++ tst/hair.js
@@ -0,0 +1,17 @@
+function Hair() {
+    hair1 = loadImage('assets/hair1.png');
+    hair2 = loadImage('assets/hair2.png');
+    imageMode(CENTER);
+
+    this.x = (windowWidth / 2) * 2;
+    this.y = (windowHeight / 3) * 1.22;
+
+
+    this.show = function () {
+        if (hairArray[hairArrayPosition] == 'hair1') {
+            image(hair1, this.x, this.y);
+        } else if (hairArray[hairArrayPosition] == 'hair1') {
+            image(hair2, this.x, this.y);
+        }
+    }
+}
--- tst/sketch.js
+++ tst/sketch.js
@@ -1,12 +1,22 @@
 var body;
 var face;
+var pattern;
+var hair;
 var faceArray = ['face1', 'face2', 'face3'];
 var faceArrayPosition = 0;
+
+var hairArray = ['hair1', 'hair2'];
+var hairArrayPosition = 0;
+
+var patternArray = ['pattern1', 'pattern2', 'pattern3'];
+var patternArrayPosition = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     body = new Body();
     face = new Face();
+    hair = new Hair();
+    pattern = new Pattern();
     next_arrow = loadImage('assets/next_arrow.png');
     prev_arrow = loadImage('assets/prev_arrow.png');
 }
@@ -16,8 +26,10 @@ function draw() {
     image(next_arrow, windowWidth / 2 + 230, windowHeight / 2);
     image(prev_arrow, windowWidth / 2 - 230, windowHeight / 2);
     scale(.5);
+    pattern.show();
     body.show();
     face.show();
+    hair.show();
 
 }
 
